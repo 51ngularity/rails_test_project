d319025582551dc76dd5ec939dcb65221eeedbc3
Sergey Nenaschew <sergey@42he.com>
2020-06-02 14:03:48 +0200

    add product model; add validations
    - validations:
    	- presence and length for name and description
    
    - rubocop:
    	- undealt with complaints

---
+++ app/models/product.rb
@@ -0,0 +1,4 @@
+class Product < ApplicationRecord 
+  validates :name, presence: true, length: { minimum: 3, maximum: 100}
+  validates :description, presence: true, length: { minimum: 10, maximum: 5000}
+end
