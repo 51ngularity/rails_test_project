ff74810e4b2fc65b34578f1116c5c8e0e8bf1b03
Sergey Nenaschew <sergey@42he.com>
2020-06-24 03:50:14 +0200

    Add superficial changes to models/product_test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -3,15 +3,15 @@
 require 'test_helper'
 
 class ProductTest < ActionDispatch::IntegrationTest
-  test 'should save valid article' do
+  test 'should save valid product' do
     product = products(:valid_product)
-    assert product.save
+    assert product.save, 'did not save valid product'
   end
 
   test 'should reject saving if name empty' do
     product = products(:valid_product)
     product.name = nil
-    assert_not product.save
+    assert_not product.save, 'saved with empty name'
   end
 
   test 'should reject saving if name has wrong length' do
