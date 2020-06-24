1ce1651e93b7444a0f4592d77bd2edfe1f7ce04a
Sergey Nenaschew <sergey@42he.com>
2020-06-24 03:52:04 +0200

    Add superficial changes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'products#redirect_to_index'
-
   resources :products
+  
   get 'bulma_layouts', to: 'bulma#bulma_layouts'
   get 'bulma_elements', to: 'bulma#bulma_elements'
 end
