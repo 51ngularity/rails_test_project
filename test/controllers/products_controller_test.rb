e592ada5368e252dfac207ba3e3c091e5abffeec
Sergey Nenaschew <sergey@42he.com>
2020-06-28 13:53:26 +0200

    Create a random number of products for products controller index test

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -72,6 +72,9 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 # testing views
 
   test 'testing index elements' do
+    (0...rand(5)).each do |x|
+      Product.create(name: "product name #{x}", description: "product description #{x}")
+    end
     get products_path
     assert_select 'div', 'Produktliste'
     assert_select 'table' do
