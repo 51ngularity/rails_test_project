e071763d22f9fe673c0a68ae12c4f082eeb5fa68
Sergey Nenaschew <sergey@42he.com>
2020-07-12 17:24:20 +0200

    Add flash notifications for (un)successfully creating/editing a product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,12 +14,18 @@ class ProductsController < ApplicationController
   end
 
   def new 
+    @product = Product.new
   end
 
   def create
     @product = Product.new(params.require(:product).permit(:name, :description))
-    @product.save
+    if @product.save
+      flash[:notice] = 'Product-entry was saved successfully'
     redirect_to product_path(@product.id)
+    else
+      flash[:alert] = 'Product-entry could not be saved due to errors'
+      render 'new'
+  end
   end
 
   def edit
@@ -28,8 +34,13 @@ class ProductsController < ApplicationController
 
   def update
     @product = Product.find(params[:id])
-    @product.update(params.require(:product).permit(:name, :description))
+    if @product.update(params.require(:product).permit(:name, :description))
+      flash[:notice] = 'Product-entry was updated successfully'
     redirect_to product_path(@product.id)
+    else
+      flash[:alert] = 'Product-entry could not be updated due to errors'
+      render 'edit'
+    end
   end
 
   def destroy
