108251681d518634edfbc752dd292075c94e31f7
Sergey Nenaschew <sergey@42he.com>
2020-07-12 17:10:17 +0200

    Add "unique" validation to name attribute in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,6 @@
 
 # class documentation comment placeholder
 class Product < ApplicationRecord
-  validates :name, presence: true, length: { minimum: 3, maximum: 100 }
+  validates :name, presence: true, length: { minimum: 3, maximum: 100 }, uniqueness: { case_sensitive: false }
   validates :description, presence: true, length: { minimum: 10, maximum: 5000 }
 end
